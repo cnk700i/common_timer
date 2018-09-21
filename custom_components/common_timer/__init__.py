6d5a093f8f962b6f20b1b1ab2c0cb759e5fce9da
cnk700i <39141120+cnk700i@users.noreply.github.com>
2018-09-21 09:47:38 +0800

    Update __init__.py
    增加点击排序、保存任务信息

--- custom_components/common_timer/__init__.py
+++ custom_components/common_timer/__init__.py
@@ -37,7 +37,6 @@ _LOGGER = logging.getLogger(__name__)
 TIME_BETWEEN_UPDATES = timedelta(seconds=1)
 STORAGE_VERSION = 1
 STORAGE_KEY = 'common_timer_tasks'
-
 DOMAIN = 'common_timer'
 DEPENDENCIES = ['group']
 ENTITY_ID_FORMAT = DOMAIN + '.{}'
